e9b18666e89bbd78241987d5e4b5314fc4d664ad
saltywitch <98076828+saltywitch@users.noreply.github.com>
2024-08-08 13:09:38 +0800

    Add files via upload

---
+++ story_content/triggers.js
@@ -0,0 +1,18 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "68GGdH0E4sh":
+        Script1();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+};
---
+++ story_content/user.js
@@ -0,0 +1,14 @@
+window.InitUserScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+window.Script1 = function()
+{
+  if(void 0===window.stencilsheets){t="https://cluelabs.com/stencil/display/widget-sheets-init-v2",s={authtool:"sl",chart:"MTk4OHw1MzQ3fDE0ZGFhNTEzNmE3YzBmNjQ2YjZkZTEzMThjM2Q1NDE0"},a=Object.keys(s).map(e=>encodeURIComponent(e)+"="+encodeURIComponent(s[e])).join("&");(xhttp=new XMLHttpRequest).onreadystatechange=function(){if(4==this.readyState&&200==this.status&&""!=this.responseText){var e=this.responseText,t=document.getElementsByTagName("head")[0],s=document.createElement("script");t.appendChild(s),s.appendChild(document.createTextNode(e)),window.stencilsheets=!0}},xhttp.open("POST",t,!0),xhttp.setRequestHeader("Content-Type","application/x-www-form-urlencoded"),xhttp.send(a)}
+        
+}
+
+};
